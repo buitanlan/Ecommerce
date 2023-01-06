ce967f0d8eea3ba7fe59a77fed8980bb2a2678cd
Bùi Tấn Lân <tanlanhcmus@gmail.com>
2023-01-07 00:43:21 +0700

    feat(): init

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <abp-loader-bar></abp-loader-bar>
-    <abp-dynamic-layout></abp-dynamic-layout>
+    <div>Hello World</div>
   `,
+  standalone: true
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'angular';
+}
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,13 +1,11 @@
-import { enableProdMode } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.route';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [ importProvidersFrom(RouterModule.forRoot(routes))]
+})
