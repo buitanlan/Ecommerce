003556f4cc22d7b196a1843f8cfc83f7dfb1641b
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2023-02-16 18:42:42 +0700

    feat(): add crud abp

---
+++ aspnet-core/src/Ecommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Ecommerce.Admin.ProductCategories;
+
+public interface IProductCategoriesAppService : ICrudAppService
+    <ProductCategoryDto,
+        Guid, 
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto, 
+        CreateUpdateProductCategoryDto>
+{
+}
---
+++ aspnet-core/src/Ecommerce.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ecommerce.ProductCategories;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Ecommerce.Admin.ProductCategories;
+
+public class ProductCategoriesAppService: CrudAppService<
+    ProductCategory,
+    ProductCategoryDto,
+    Guid,
+    PagedResultRequestDto,
+    CreateUpdateProductCategoryDto,
+    CreateUpdateProductCategoryDto>, IProductCategoriesAppService
+{
+    public ProductCategoriesAppService(IRepository<ProductCategory, Guid> repository)
+        : base(repository)
+    {
+    }
+}
