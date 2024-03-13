16c547f6c3b5b7504dc65f69762d823c98f58d09
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2024-03-13 14:17:45 +0700

    feat(): add footer and header view component

---
+++ aspnet-core/src/Ecommerce.Public.Web/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Public.Web.ViewComponents;
+
+public class HeaderViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync()
+    {
+        return Task.FromResult<IViewComponentResult>(View());
+    }
+}
