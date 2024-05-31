70da1ee275accee774508abbad5ce9274e629354
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2024-05-31 17:55:17 +0700

    feat: update angular 18

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,31 +1,33 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { registerLocale } from '@abp/ng.core/locale';
 import { environment } from '../environments/environment';
 import { CoreModule } from '@abp/ng.core';
 import { tokenInterceptor } from './shared/interceptors/token.interceptor';
-import { DialogService } from 'primeng/dynamicdialog';
-import { MessageService } from 'primeng/api';
 import { NotificationService } from './shared/services/notification.service';
 import { UtilityService } from './shared/services/utility.service';
 import { errorHandlerInterceptor } from './shared/interceptors/error-handler.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     importProvidersFrom(
       RouterModule.forRoot(routes),
-      BrowserAnimationsModule,
       CoreModule.forRoot({
         environment,
         registerLocaleFn: registerLocale(),
       }),
     ),
-    provideHttpClient(withInterceptors([tokenInterceptor, errorHandlerInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor, errorHandlerInterceptor])),
     DialogService,
     MessageService,
+    ConfirmationService,
     NotificationService,
     UtilityService,
   ],
